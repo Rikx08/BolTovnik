411a3239547d0864a88ce4302f75d238acad831d
Rikx08 <73607438+Rikx08@users.noreply.github.com>
2022-12-07 23:58:39 +0300

    ОШИБКА!!!!!!!!!!!!!!!!!!!!
    ОН ПОТЕРЯЛСЯ

---
+++ BolTovnik/app/src/main/java/com/rikx/boltovnik/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,10 @@
+package com.rikx.boltovnik.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+
+}
